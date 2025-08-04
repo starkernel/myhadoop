e9ddb0d2ca1cfb5e3d3d86ee42ff80494f9edc52
JaneTTR <14683572+janettr@user.noreply.gitee.com>
2025-08-04 15:51:31 +0800

    feature: [ubuntu22] 解决修复 doris 问题

--- scripts/build/ambari3/ub2204/build_ambari_all.sh
+++ scripts/build/ambari3/ub2204/build_ambari_all.sh
@@ -37,6 +37,7 @@ patch_files=(
   "/scripts/build/ambari3/common/patch2_1_0/patch0-DEBIAN-BASE-SUP.diff"
   "/scripts/build/ambari3/common/patch2_1_0/patch1-DEBIAN-BASE-SUP.diff"
   "/scripts/build/ambari3/common/patch2_1_0/patch2-DEBIAN-DOLPHIN-RANGER-SUP.diff"
+  "/scripts/build/ambari3/common/patch2_1_0/patch3-DEBIAN-DORIS-SUP.diff"
   # 后续可继续添加补丁文件路径
 )
 
