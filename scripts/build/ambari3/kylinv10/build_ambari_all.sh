7928b39ab3bfd0836910533c0a3836accf8dcf41
JaneTTR <14683572+janettr@user.noreply.gitee.com>
2025-10-09 10:54:22 +0800

    feature: [kylin-v10-x86_64] 引入组件支持kylin v 10

--- scripts/build/ambari3/kylinv10/build_ambari_all.sh
+++ scripts/build/ambari3/kylinv10/build_ambari_all.sh
@@ -37,8 +37,9 @@ patch_files=(
   "/scripts/build/ambari3/common/patch2_1_0/patch1-DEBIAN-BASE-SUP.diff"
   "/scripts/build/ambari3/common/patch2_1_0/patch2-DEBIAN-DOLPHIN-RANGER-SUP.diff"
   "/scripts/build/ambari3/common/patch2_1_0/patch3-DEBIAN-FINAL-SUP.diff"
-  # 2.1.0 补丁
+  # 2.2.0 补丁
   "/scripts/build/ambari3/common/patch2_2_0/patch0-KYLIN-BASE-SUP.diff"
+  "/scripts/build/ambari3/common/patch2_2_0/patch1-KYLIN-ALL-COMPONENT-SUP.diff"
 
   # 后续可继续添加补丁文件路径
 
