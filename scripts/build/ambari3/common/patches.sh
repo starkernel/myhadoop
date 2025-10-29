7d148050412492ab767411c018c54480497acfa8
JaneTTR <14683572+janettr@user.noreply.gitee.com>
2025-10-29 16:12:50 +0800

    fixed：修复ambari 3.0 的 kerberos 无法开启的问题。

---
+++ scripts/build/ambari3/common/patches.sh
@@ -0,0 +1,20 @@
+# 单点维护补丁清单（按顺序）
+# 用绝对/相对路径都行，建议使用 repo 根为基准的相对路径
+patch_files=(
+  "/scripts/build/ambari3/common/patch2_0_0/patch0-ALL-IN-ONE.diff"
+  "/scripts/build/ambari3/common/patch2_0_0/patch1-START-LOGBACK-LOGGER.diff"
+  # 2.1.0
+  "/scripts/build/ambari3/common/patch2_1_0/patch0-DEBIAN-BASE-SUP.diff"
+  "/scripts/build/ambari3/common/patch2_1_0/patch1-DEBIAN-BASE-SUP.diff"
+  "/scripts/build/ambari3/common/patch2_1_0/patch2-DEBIAN-DOLPHIN-RANGER-SUP.diff"
+  "/scripts/build/ambari3/common/patch2_1_0/patch3-DEBIAN-FINAL-SUP.diff"
+  # 2.2.0
+  "/scripts/build/ambari3/common/patch2_2_0/patch0-KYLIN-BASE-SUP.diff"
+  "/scripts/build/ambari3/common/patch2_2_0/patch1-KYLIN-ALL-COMPONENT-SUP.diff"
+  # 2.2.1
+  "/scripts/build/ambari3/common/patch2_2_1/patch0-KERBEROS-SUP-FIXED.diff"
+  "/scripts/build/ambari3/common/patch2_2_1/patch1-ATLAS-RHEL-FIXED.diff"
+  "/scripts/build/ambari3/common/patch2_2_1/patch2-VIEWS-COMPILE-FIXED.diff"
+)
+
+
